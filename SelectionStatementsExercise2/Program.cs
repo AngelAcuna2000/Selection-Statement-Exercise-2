9e4cb67f4e36a361037eb396d6fd085c14b25ae1
Angel Acuna <AngelAcuna2000@gmail.com>
2023-05-15 16:04:48 -0400

    asks user for favorite school subject and prints a custom message based on what user inputs

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+
+            var schoolSubject = Console.ReadLine();
+
+            switch (schoolSubject.ToLower())
+            {
+                case "math":
+                    {
+                        Console.WriteLine("Your favorite subject is math");
+                    }
+                    break;
+
+                case "science":
+                    {
+                        Console.WriteLine("Your favorite subject is math");
+                    }
+                    break;
+
+                case "pe":
+                    {
+                        Console.WriteLine("Your favorite subject is math");
+                    }
+                    break;
+
+                case "english":
+                    {
+                        Console.WriteLine("Your favorite subject is math");
+                    }
+                    break;
+
+                case "history":
+                    {
+                        Console.WriteLine("Your favorite subject is math");
+                    }
+                    break;
+
+                default:
+                    {
+                        Console.WriteLine($"Your favorite subject is {schoolSubject}");
+                    }
+                    break;
+            }
         }
     }
 }
